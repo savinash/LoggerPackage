3434c9ae454b5fa6322a1ccb7d8b85ba46874d1a
AvinashVardhan <avinash.vardhan@888holdings.com>
2023-10-17 17:11:09 +0100

    Uploading Example

---
+++ Example/LoggerExample/LoggerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoggerExampleApp.swift
+//  LoggerExample
+//
+//  Created by Avinash Vardhan on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LoggerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
