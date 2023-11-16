1ea6d51a9ae04357658ecf49fa631f4483564724
AvinashVardhan <avinash.vardhan@888holdings.com>
2023-11-16 15:52:50 +0000

    update

--- Example/LoggerExample/ContentView.swift
+++ Example/LoggerExample/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, worldasfd!")
         }
         .padding()
     }
