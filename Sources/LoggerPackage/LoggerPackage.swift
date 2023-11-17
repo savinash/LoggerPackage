aa61b84069c826c02e7c4ffd0831d935794b8376
AvinashVardhan <avinash.vardhan@888holdings.com>
2023-11-17 10:52:57 +0000

    update

--- Sources/LoggerPackage/LoggerPackage.swift
+++ Sources/LoggerPackage/LoggerPackage.swift
@@ -1,2 +1,36 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+@available(iOS 15.0, *)
+public struct MultiTarget: View {
+    @State private var showingAlert = false
+
+    public var body: some View {
+        Text("Swift View Multiple Target")
+        Button("Show Alert") {
+                    showingAlert = true
+                }
+        
+        .alert("Important message", isPresented: $showingAlert) {
+                    Button("OK", role: .cancel) { }
+                }
+    }
+    
+    
+    
+    func showMessage() {
+        print("Test message")
+    }
+}
+
+#Preview {
+    if #available(iOS 15.0, *) {
+        MultiTarget()
+    } else {
+        // Fallback on earlier versions
+    }
+}
+
+
